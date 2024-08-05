ca0702f4a87c26997b52229b54511816a904e57a
Pedro Henrique Panstein <phfarof@gmail.com>
2024-08-05 18:30:28 -0300

    início da calculadora de imc  18:30 - 05/08/2024

---
+++ JavaScript/PROJETOS/CLONE_IPHONE_13/js/scripts.js
@@ -0,0 +1,27 @@
+const button = document.querySelectorAll('#image-picker li')
+const phoneImage = document.querySelector('#product-image')
+
+button.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    console.log(e)
+
+    button.forEach((btn) => {
+      btn.querySelector('.color').classList.remove('selected')
+    })
+
+    const buttons = e.target
+
+    const id = buttons.getAttribute('id')
+    console.log(id)
+
+    buttons.querySelector('.color').classList.add('selected')
+    phoneImage.classList.toggle('changing')
+    phoneImage.setAttribute('src', `img/iphone_${id}.jpg`)
+    
+    setTimeout(() => {
+      phoneImage.classList.toggle('changing')
+    }, 200)
+
+  })
+})
+
