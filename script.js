148713ac5c6bbc72762b6534ee15a402e75abe2d
Pedro Henrique Panstein <phfarof@gmail.com>
2024-08-21 16:28:04 -0300

    Treinando a mudança de tema com js  16:27  21/08/2024

---
+++ script.js
@@ -0,0 +1,19 @@
+const themeButton = document.getElementById("theme");
+
+themeButton.addEventListener("click", () => {
+  const variavel = document.documentElement;
+  const darkTheme =
+    getComputedStyle(variavel).getPropertyValue("--fundo") === "#333";
+
+  if (darkTheme) {
+    variavel.style.setProperty("--fundo", "#fafafa");
+    variavel.style.setProperty("--texto", "#000")
+    themeButton.innerHTML = "Claro";
+    console.log("Mudou para branco");
+  } else {
+    variavel.style.setProperty("--fundo", "#333")
+    variavel.style.setProperty("--texto", "#fff")
+    themeButton.innerHTML = "Escuro";
+    console.log("Mudou para preto");
+  }
+});
