6dbdb592736671ee9bd53b16cbbb101c6946878f
pedro <phfarof@gmail.com>
2024-08-23 23:25:18 -0300

    Mudança de lingua finalizado  23:25 - 23/08/2024

--- JavaScript/TESTANDO_TEMA/script.js
+++ JavaScript/TESTANDO_TEMA/script.js
@@ -10,22 +10,20 @@ button.forEach((btn) => {
 
 const themeButton = document.getElementById("theme");
 const root = document.documentElement;
+const notification = document.querySelector(".notification");
+const texto = document.getElementById("texto");
+const themeImage = document.getElementById("theme-image");
 
 themeButton.addEventListener("click", () => {
   // const darkTheme = root.style.getPropertyValue("--fundo-preto") === "#333";
-  const darkTheme =
-    getComputedStyle(root).getPropertyValue("--fundo-preto") === "#333";
-  const iconSun = `<img src="assets/iconSun.png">`;
-  const iconMoon = `<img src="assets/iconMoon.png">`;
-  const notification = document.querySelector(".notification");
-  const texto = document.getElementById("texto");
+  const darkTheme = getComputedStyle(root).getPropertyValue("--fundo-preto") === "#333";
 
   if (darkTheme) {
     root.style.setProperty("--fundo-preto", "#f5f5f5");
     root.style.setProperty("--texto-preto", "#333");
     root.style.setProperty("--texto-branco", "#000");
     root.style.setProperty("--fundo-botao", "#d4d4d4");
-    themeButton.innerHTML = `Mudar tema ${iconMoon}`;
+    themeImage.setAttribute("src", "assets/iconMoon.png");
     console.log("Mudou para branco");
     texto.innerHTML = "Tema alterado para: Claro";
   } else {
@@ -33,7 +31,7 @@ themeButton.addEventListener("click", () => {
     root.style.setProperty("--texto-preto", "#fff");
     root.style.setProperty("--texto-branco", "#fff");
     root.style.setProperty("--fundo-botao", "#525252");
-    themeButton.innerHTML = `Mudar tema ${iconSun}`;
+    themeImage.setAttribute("src", "assets/iconSun.png");
     console.log("Mudou para preto");
     texto.innerHTML = "Tema alterado para: Escuro";
   }
@@ -54,45 +52,76 @@ const img = document.querySelector("img");
 
 font12.addEventListener("click", () => {
   root.style.setProperty("--fonte", "12px");
-  img.style.setProperty("height", "22px")
-  img.style.setProperty("width", "22px")
+  img.style.setProperty("height", "22px");
+  img.style.setProperty("width", "22px");
 });
 
 font14.addEventListener("click", () => {
   root.style.setProperty("--fonte", "14px");
-  img.style.setProperty("height", "24px")
-  img.style.setProperty("width", "24px")
+  img.style.setProperty("height", "24px");
+  img.style.setProperty("width", "24px");
 });
 
 font16.addEventListener("click", () => {
   root.style.setProperty("--fonte", "16px");
-  img.style.setProperty("height", "26px")
-  img.style.setProperty("width", "26px")
+  img.style.setProperty("height", "26px");
+  img.style.setProperty("width", "26px");
 });
 
 font18.addEventListener("click", () => {
   root.style.setProperty("--fonte", "18px");
-  img.style.setProperty("height", "28px")
-  img.style.setProperty("width", "28px")
+  img.style.setProperty("height", "28px");
+  img.style.setProperty("width", "28px");
 });
 
 font20.addEventListener("click", () => {
   root.style.setProperty("--fonte", "20px");
-  img.style.setProperty("height", "30px")
-  img.style.setProperty("width", "30px")
+  img.style.setProperty("height", "30px");
+  img.style.setProperty("width", "30px");
 });
 
-const menuLanguages = document.getElementById("arrow")
-const allFlags = document.querySelector(".all-flags")
+const menuLanguages = document.getElementById("arrow");
+const allFlags = document.querySelector(".all-flags");
 const portuguese = document.getElementById("portuguese");
 const english = document.getElementById("english");
 const germany = document.getElementById("germany");
-const actualImg = document.getElementById("actualImg")
+const actualImg = document.getElementById("actualImg");
+const buttonTheme = document.querySelector("#theme span")
+const buttonSelect = document.querySelectorAll(".color span")
+const titleFont = document.querySelector(".font-size h1")
 
 menuLanguages.addEventListener("click", () => {
-  allFlags.classList.toggle("hidden")
-})
+  allFlags.classList.toggle("hidden");
+});
 
 english.addEventListener("click", () => {
-  actualImg.setAttribute("src", "assets/eua.png")
-})
+  actualImg.setAttribute("src", "assets/eua.png");
+  allFlags.classList.add("hidden");
+  buttonTheme.innerHTML = "Change theme"
+  buttonSelect.forEach(span => {
+    span.innerHTML = "Button";
+  })
+  titleFont.innerHTML = "Change font size"
+});
+
+portuguese.addEventListener("click", () => {
+  actualImg.setAttribute("src", "assets/brasil.jpg");
+  allFlags.classList.add("hidden");
+  buttonTheme.innerHTML = "Mudar tema"
+  buttonSelect.forEach(span => {
+    span.innerHTML = "Botão";
+  })
+  titleFont.innerHTML = "Mudar tamanho da fonte"
+});
+
+germany.addEventListener("click", () => {
+  actualImg.setAttribute("src", "assets/alemanha.png");
+  allFlags.classList.add("hidden");
+  buttonTheme.innerHTML = "Cambiar tema"
+  buttonSelect.forEach(span => {
+    span.innerHTML = "Botón";
+  })
+  titleFont.innerHTML = "Cambiar el tamaño de fuente"
+});
+
+//MUDAR BANDEIRA DA ALEMANHA PARA A DA ESPANHA
