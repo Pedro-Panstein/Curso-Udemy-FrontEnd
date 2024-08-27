2ee134bfdd2e67efed7e3dfb3908c7f9b087c77f
Pedro Henrique Panstein <phfarof@gmail.com>
2024-08-27 17:00:28 -0300

    Continuação do dev notes, imprementação do js 17:00 - 27/08/2024

--- JAVASCRIPT/PROJETOS/DEV-NOTES/js/script.js
+++ JAVASCRIPT/PROJETOS/DEV-NOTES/js/script.js
@@ -2,8 +2,22 @@ const notesContainer = document.querySelector("#notes-container");
 const noteInput = document.querySelector("#note-content");
 const addNoteBtn = document.querySelector(".add-note");
 
+function showNotes() {
+  cleanNotes();
+
+  getNotes().forEach((note) => {
+    const noteElement = createNote(note.id, note.content, note.fixed);
+
+    notesContainer.appendChild(noteElement);
+  });
+}
+
+function cleanNotes() {
+  notesContainer.replaceChildren([])
+}
+
 function addNote() {
-  const notes = [];
+  const notes = getNotes();
 
   const noteObject = {
     id: generateId(),
@@ -15,6 +29,10 @@ function addNote() {
   notesContainer.appendChild(noteElement);
 
   notes.push(noteObject);
+
+  saveNotes(notes);
+
+  noteInput.value = "";
 }
 
 function generateId() {
@@ -29,7 +47,45 @@ function createNote(id, content, fixed) {
   textArea.placeholder = "Adicione algum texto...";
   element.appendChild(textArea);
 
+  const pinIcon = document.createElement("i");
+
+  pinIcon.classList.add(...["bi", "bi-pin"]);
+  element.appendChild(pinIcon);
+
+  if (fixed) {
+    element.classList.add("fixed");
+  }
+
+  element.querySelector(".bi-pin").addEventListener("click", () => {
+    toggleFixNote(id);
+  });
+
   return element;
 }
 
+function toggleFixNote(id) {
+  const notes = getNotes();
+  const targetNotes = notes.filter((note) => note.id === id)[0];
+
+  targetNotes.fixed = !targetNotes.fixed;
+
+  saveNotes(notes);
+
+  showNotes();
+}
+
+function getNotes() {
+  const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+ 
+  const orderedNotes = notes.sort((a, b) => a.fixed > b.fixed ? -1 : 1);
+ 
+  return notes;
+}
+
+function saveNotes(notes) {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
 addNoteBtn.addEventListener("click", () => addNote());
+
+showNotes();
